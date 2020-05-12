6358d62e666d93fa475e7a82cbc2fdebb3cf6f97
Muhammad Naeem <naeem0313@gmail.com>
2020-05-12 19:52:57 +0500

    Completed viewmodel.

--- app/src/main/java/com/appsolace/androidtask/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/appsolace/androidtask/viewmodel/UserViewModel.kt
@@ -1,4 +1,22 @@
 package com.appsolace.androidtask.viewmodel
 
-class UserViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.appsolace.androidtask.model.UserResponseModel
+import com.appsolace.androidtask.repo.Repository
+import com.appsolace.androidtask.repo.ResponseListener
+
+class UserViewModel (application: Application) : AndroidViewModel(application) {
+    private val mRepository: Repository
+
+    fun getusers( listener: ResponseListener): MutableLiveData<List<UserResponseModel>> {
+        return mRepository.get_users(listener)
+    }
+
+    init {
+        mRepository = Repository()
+    }
+
+
 }

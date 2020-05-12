e0f36f4b2c48a01d3af957fd4db7a363a3958f85
Muhammad Naeem <naeem0313@gmail.com>
2020-05-12 18:45:24 +0500

    Create funcation for checking internet connection

--- app/src/main/java/com/appsolace/androidtask/utils/Constants.kt
+++ app/src/main/java/com/appsolace/androidtask/utils/Constants.kt
@@ -1,5 +1,38 @@
 package com.appsolace.androidtask.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
 val BASE_URL = "https://jsonplaceholder.typicode.com/"
 
+
+//   for for checking internet  available ot not
+fun isNetworkAvailable(context: Context?): Boolean {
+    if (context == null) return false
+    val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        if (capabilities != null) {
+            when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> {
+                    return true
+                }
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> {
+                    return true
+                }
+            }
+        }
+    } else {
+        val activeNetworkInfo = connectivityManager.activeNetworkInfo
+        if (activeNetworkInfo != null && activeNetworkInfo.isConnected) {
+            return true
+        }
+    }
+    return false
+}
 

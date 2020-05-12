cfe259aa96558da46b298a8a4dfacaaffac9330e
Muhammad Naeem <naeem0313@gmail.com>
2020-05-12 20:01:36 +0500

    Calling APi in view

--- app/src/main/java/com/appsolace/androidtask/MapsActivity.kt
+++ app/src/main/java/com/appsolace/androidtask/MapsActivity.kt
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.appsolace.androidtask.model.UserResponseModel
+import com.appsolace.androidtask.repo.ResponseListener
+import com.appsolace.androidtask.utils.isNetworkAvailable
+import com.appsolace.androidtask.viewmodel.UserViewModel
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -11,22 +18,30 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.util.ArrayList
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback ,ResponseListener{
 
     private lateinit var mMap: GoogleMap
+    var mUserResponseModellist: MutableList<UserResponseModel> = ArrayList<UserResponseModel>()
+    var viewModel: UserViewModel?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //making this activity full screen
         window.requestFeature(Window.FEATURE_NO_TITLE)
-      //  window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        viewModel= ViewModelProvider(this).get(UserViewModel::class.java)
+        if (isNetworkAvailable(this)){
+            //calling ai from view and get response
+            viewModel!!.getusers(this).observe(this, Observer { mlistuser->
+                mUserResponseModellist.addAll(mlistuser as Array<out UserResponseModel>)
+            })
+        }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -36,5 +51,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }

166edfa2fb9eb98d962727680c6f1a075427cef0
Muhammad Naeem <naeem0313@gmail.com>
2020-05-12 19:41:17 +0500

    Adding Users api into interface

--- app/src/main/java/com/appsolace/androidtask/network_retrofit/ApiDataService.kt
+++ app/src/main/java/com/appsolace/androidtask/network_retrofit/ApiDataService.kt
@@ -1,4 +1,12 @@
 package com.appsolace.androidtask.network_retrofit
 
+import com.appsolace.androidtask.model.UserResponseModel
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface ApiDataService {
+
+    @GET("users")
+    fun getusers( ): Call<List<UserResponseModel>>?
+
 }
